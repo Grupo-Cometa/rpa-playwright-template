1198b89ce9b961776351ed485ce1bc138ae9bfb9
Pedro Souza <pedroh.olives@gmail.com>
2023-09-14 12:58:36 -0400

    feat: 1.0.10

--- cli.js
+++ cli.js
@@ -15,29 +15,32 @@ if (projectName) {
 
 fs.copySync(templatePath, projectPath);
 
-// Personalizar o package.json, se projectName for fornecido
 if (projectName || currentDirectoryName !== "template") {
   const packageJsonPath = path.join(projectPath, 'package.json');
   const packageJson = require(packageJsonPath);
 
-  // Verificar se já existe um package.json no diretório atual
   const currentPackageJsonPath = path.join(currentDirectory, 'package.json');
 
   if (fs.existsSync(currentPackageJsonPath)) {
-    // Se já existe um package.json, mescla as propriedades
     const currentPackageJson = require(currentPackageJsonPath);
     Object.assign(packageJson, currentPackageJson);
   }
 
-  // Substituir o marcador de posição pelo nome do projeto
   packageJson.name = projectName || currentDirectoryName;
-
-  // Salvar o package.json personalizado de volta
   fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
 
-if (projectName || currentDirectoryName !== "template") {
-  console.log('\x1b[32m%s\x1b[0m', `${projectPath} criado com sucesso a partir do template RPA!`);
-} else {
-  console.log('\x1b[32m%s\x1b[0m', `Template RPA copiado com sucesso para o diretório atual!`);
-}
+const gitIgnoreContent = `
+node_modules/
+test-results/
+playwright-report/
+playwright/.cache/
+.env
+.vscode/
+dist/
+storage/
+`;
+
+fs.writeFileSync(path.join(projectPath, '.gitignore'), gitIgnoreContent);
+
+console.log('\x1b[32m%s\x1b[0m', `O Template RPA foi criado com sucesso!`);
