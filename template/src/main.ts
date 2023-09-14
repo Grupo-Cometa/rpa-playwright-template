44a29c881f945fb9e47753568253202b4f75cd70
Pedro Souza <pedroh.olives@gmail.com>
2023-09-14 13:09:38 -0400

    chore: update

---
+++ template/src/main.ts
@@ -0,0 +1,13 @@
+import { Triggers, Status } from 'client-rpa-orchestrator-ts';
+
+Status.publish();
+
+const triggers = new Triggers();
+
+triggers.start(async () => {
+    
+})
+
+triggers.stop(async () => {
+    
+})
