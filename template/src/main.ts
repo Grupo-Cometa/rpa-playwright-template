f84448106581fcb29c2fb376ca159bb19596e198
Pedro Souza <pedroh.olives@gmail.com>
2023-09-15 15:29:44 -0400

    CHORE: UPDATE

--- template/src/main.ts
+++ template/src/main.ts
@@ -1,12 +1,8 @@
-import { InterfaceMain, Log } from "client-rpa-orchestrator-ts";
+import { InterfaceMain } from "client-rpa-orchestrator-ts";
 
 export class Main implements InterfaceMain {
     async start(): Promise<void> {
 
-        console.log("START my")
-        // await Log.write("info", "Info da bagaceira")
-        // await Log.write("error", "Erro da bagaceira")
-        // await Log.write("success", "Sucesso da bagaceira")
-        // await Log.write("warning", "Alerta da bagaceira")
+        console.log("START RPA")
     }
 }
